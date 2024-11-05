97f15ad635f4da58fda0f504d779caaac95c03b8
Maxim <manepomnyaschy@gmail.com>
2024-11-05 18:04:06 +0300

    Описание изменений

---
+++ SoloProjects/JS/Tests/test.js
@@ -0,0 +1,12 @@
+let number = Number(prompt("enter a number"));
+
+function numberChecker() {
+	if(number >= 10) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(numberChecker());
+
